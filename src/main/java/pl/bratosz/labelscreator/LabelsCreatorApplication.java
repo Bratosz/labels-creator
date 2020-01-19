986cc3548cfae331b0824211173f45bcfefc810e
PaleEye <palej.bartosz@gmail.com>
2020-01-19 17:07:20 +0100

    First commit

---
+++ src/main/java/pl/bratosz/labelscreator/LabelsCreatorApplication.java
@@ -0,0 +1,13 @@
+package pl.bratosz.labelscreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelsCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabelsCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bratosz/labelscreator/LabelsCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bratosz.labelscreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelsCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
