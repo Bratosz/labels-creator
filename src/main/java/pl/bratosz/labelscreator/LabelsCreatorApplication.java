f4f7537891c92964e29f7959e94efe0a7cf8d245
PaleEye <palej.bartosz@gmail.com>
2020-01-20 12:29:33 +0100

    Uploading and downloading files implemented

--- src/main/java/pl/bratosz/labelscreator/LabelsCreatorApplication.java
+++ src/main/java/pl/bratosz/labelscreator/LabelsCreatorApplication.java
@@ -2,8 +2,13 @@ package pl.bratosz.labelscreator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.bratosz.labelscreator.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class LabelsCreatorApplication {
 
     public static void main(String[] args) {
