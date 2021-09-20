a564fefb72f908f9efb4d396ed48a5f522fc58bf
Bartosz Palej <palej.bartosz@gmail.com>
2021-09-20 11:21:50 +0200

    another

--- src/main/resources/static/print-by-http.js
+++ src/main/resources/static/print-by-http.js
@@ -7,7 +7,7 @@ function sendLabelsToPrinter(labelsInZPL2) {
         alert("Coś poszło nie tak");
     } else {
         $.ajax({
-            url: `https://` + printerIp + `:` + printerPort,
+            url: `http://` + printerIp + `:` + printerPort,
             method: "post",
             data: labelsInZPL2.toString(),
         });
